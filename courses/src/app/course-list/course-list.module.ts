5a646f18c8ac1e70ffe3030344607c4a9eb917de
Elena <elenaminich157@gmail.com>
2018-06-14 16:41:23 +0300

    1

---
+++ courses/src/app/course-list/course-list.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseListItemComponent } from './course-list-item/course-list-item.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CourseListComponent, CourseListItemComponent]
+})
+export class CourseListModule { }
